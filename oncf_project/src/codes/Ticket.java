d60ada46eea2aae51f7f67cd1b8f2647ec681651
Y4hi48 <yahia0854@gmail.com>
2024-01-22 13:59:12 +0100

    v5.0

--- oncf_project/src/codes/Ticket.java
+++ oncf_project/src/codes/Ticket.java
@@ -2,27 +2,13 @@ package codes;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 
 public class Ticket {
-    public static void window() {
+    public static void window(final String gd,final String ga,final String d ,final int p) {
         JFrame f = new JFrame();
         f.setTitle("ONCF");
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -41,7 +27,8 @@ public class Ticket {
         downloadButton.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e) {
-                downloadTicket(); // Call your download ticket logic here
+                 Download telecharger= new Download(gd,ga,d,p);
+                 telecharger.ticket();
             }
         });
 
@@ -49,48 +36,5 @@ public class Ticket {
         f.setVisible(true);
     }
 
-    private static void downloadTicket(String username) {
-        Connection connection = null;
-        try {
-            // Establish a database connection
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/oncf", "root", "");
-
-            // Query to retrieve user information
-            String query = "SELECT * FROM users WHERE username = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setString(1, username);
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                // Create a temporary file to store the ticket information
-                File ticketFile = File.createTempFile("train_ticket", ".txt");
-
-                // Write ticket information to the file
-                try (FileWriter writer = new FileWriter(ticketFile)) {
-                    if (resultSet.next()) {
-                        writer.write("Passenger Name: " + resultSet.getString("username") + "\n");
-                        // Add other user-specific data here
-                    } else {
-                        // User not found
-                        JOptionPane.showMessageDialog(null, "User not found.", "Error", JOptionPane.ERROR_MESSAGE);
-                        return;
-                    }
-                }
-
-                // Display a message or perform any other actions related to ticket download
-                JOptionPane.showMessageDialog(null, "Ticket downloaded successfully: " + ticketFile.getAbsolutePath());
-            }
-        } catch (SQLException | IOException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Error downloading the ticket.", "Error", JOptionPane.ERROR_MESSAGE);
-        } finally {
-            // Close the database connection in the finally block
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+    
 }
